9b7610b5ad8e16edbf9bfb8a1c70061a28535b84
Christopher Richmond <lancer1977@gmail.com>
2026-04-20 22:49:32 -0400

    Updates

--- test/ValheimIntegratedTests.cs
+++ test/ValheimIntegratedTests.cs
@@ -1,50 +1,50 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using NUnit.Framework; 
+using NUnit.Framework;
 using PolyhydraGames.Core.Test;
 using PolyhydraGames.Valheim.Plugin.Extensions;
 using ValheimRcon.Commands;
 
-namespace PolyhydraGames.RCon.Test;
-[TestFixture]
-public class ExtensionTests
+namespace PolyhydraGames.RCon.Test
 {
-    [TestCase("71000 dan you are fat", ExpectedResult = "you are fat") ]
-    public string GetRemainder(string value)
+    [TestFixture]
+    public class ExtensionTests
     {
-        var args = new CommandArgs(value.Split(" "));
-        return args.GetRemainingString(2);
-    }
-}
-[TestFixture]
-public partial class ValheimIntegratedTests
-{ 
-#pragma warning disable NUnit1032
-    public IHost App { get; set; }
-#pragma warning restore NUnit1032
-    public ValheimIntegratedTests()
-    { 
-        App = TestHelpers.GetHost((x, services) =>
+        [TestCase("71000 dan you are fat", ExpectedResult = "you are fat")]
+        public string GetRemainder(string value)
         {
-            services.AddSingleton<HttpClient>(); 
-        }); 
-    }
-    [OneTimeSetUp]
-    public async Task SetUp()
-    { 
+            var args = new CommandArgs(value.Split(" "));
+            return args.GetRemainingString(2);
+        }
     }
 
- 
-
-    [OneTimeTearDown]
-    public void TearDown()
-    { 
-    }
-    private void ProcessResult()//RestResult result, string successMessage = "", string errorMessage = "")
+    [TestFixture]
+    public class ValheimIntegratedTests
     {
-        //Console.WriteLine(result.OutgoingMessage);
-        //Console.WriteLine(result.Response);
-        //if (!result.Successful) Console.WriteLine(errorMessage);
-        //Assert.That(result.Successful, successMessage);
+        [OneTimeSetUp]
+        public async Task SetUp()
+        {
+        }
+
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+        }
+#pragma warning disable NUnit1032
+        public IHost App { get; set; }
+#pragma warning restore NUnit1032
+        public ValheimIntegratedTests()
+        {
+            App = TestHelpers.GetHost((x, services) => { services.AddSingleton<HttpClient>(); });
+        }
+
+        private void ProcessResult() //RestResult result, string successMessage = "", string errorMessage = "")
+        {
+            //Console.WriteLine(result.OutgoingMessage);
+            //Console.WriteLine(result.Response);
+            //if (!result.Successful) Console.WriteLine(errorMessage);
+            //Assert.That(result.Successful, successMessage);
+        }
     }
 }
